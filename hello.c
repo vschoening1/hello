fd4a4e6f106444397b5224c20e09c2e934783e07
Vienna Schoening <vschoening2@huskers.unl.edu>
2023-08-24 09:47:48 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Vienna Schoening
+ *         vschoening2@unl.edu
+ * Date: 2023/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Vienna\n");
+
+
+  return 0;
+}
